36dce4a7bc6661d9215d25b7dd5c8f062582fcc8
Ernest-D <daphnee.ernest@yahoo.com>
2025-08-11 18:15:30 +0200

    css navbar

---
+++ mon-projet/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useState } from 'react';
+import styles from "./NavBar.module.css";
+import DylanAvatar from '../../assets/icons/DylanAvatar.png'
+import barMenu from '../../assets/icons/barMenu.png'
+
+
+function Navbar() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const toggleMenu = () => setIsOpen(prev => !prev);
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+               <a href="#home"><img src={DylanAvatar} alt="Logo" /></a>
+            </div>
+            <a href="#menu" className={styles.burger} onClick={toggleMenu}><img src={barMenu} alt="Menu" /></a>
+            <ul className={`${styles.menu} ${isOpen ? styles.menuOpen : ''}`}>
+                <li><a href='#about'>À propos</a></li>
+                <li><a href='#projects'>Mes projets</a></li>
+                <li><a href='#contact'>Contact</a></li>
+                </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
